35e85b85c45dabde599fa909b61474f2f0eb6208
Anupam Jain <anupam2204.smart@gmail.com>
2015-11-21 18:46:41 +0530

    Addded code for link extraction from python file

--- parser/codeParser.py
+++ parser/codeParser.py
@@ -10,24 +10,37 @@ class CodeParser(object):
         """
         initialize
         """
-        self.pattern1 = r'/*'
-        self.pattern2 = r'*/'
+        self.java_comment_start = r'/*'
+        self.java_comment_end = r'*/'
+        self.python_comment = r"'''"
 
     def get_page_link(self, file_name):
         """
         Get the link to the problem description page from code file.
+        Returns None if no such link is found
         :param file_name: code file to be parsed
         :return: link (if any) to the problem page
         """
-        if '.java' in file_name:
-            with open(file_name, 'r') as inf:
+        with open(file_name, 'r') as inf:
+            if '.java' in file_name:
                 line = inf.readline()
-                # print line
-                if self.pattern1 in line:
+                if self.java_comment_start in line:
                     print file_name + " : " + line
-                    link = line[line.index(self.pattern1) + 2: line.index(self.pattern2)]
+                    link = line[line.index(self.java_comment_start) +
+                                len(self.java_comment_start): line.index(self.java_comment_end)]
                     # print link
                     return link.strip()
+            elif '.py' in file_name:
+                line = inf.readline()
+                # print line
+                if self.python_comment in line:
+                    print file_name + " : " + line
+                    link = line[line.index(self.python_comment) +
+                                len(self.python_comment): line.index(self.python_comment)]
+                    # print link
+                    return link.strip()
+
+            return None
 
     def get_code(self, file_name):
         """
