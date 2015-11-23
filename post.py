8cd22b3ef4d9d56de3de03601bacadbaffaaaee1
Anupam Jain <anupam2204.smart@gmail.com>
2015-11-24 00:02:56 +0530

    Updated the class to reflect inbuilt variable usage

--- post.py
+++ post.py
@@ -5,4 +5,9 @@ Plain object to hold and transfer post related data.
 
 class Post(object):
     def __init__(self, **kwargs):
+        """
+        :param kwargs: host_name, link, raw_page, file_name, title,
+        problem_text, files_covered, post_file
+        :return:
+        """
         self.__dict__ = kwargs
