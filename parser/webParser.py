ba560adfddc0181f561e008767702c9a05e595fb
Anupam Jain <anupam2204.smart@gmail.com>
2016-02-28 19:17:47 +0530

    GeekforGeek parser: integration done with proper comments

--- parser/webParser.py
+++ parser/webParser.py
@@ -3,6 +3,7 @@ Controller to forward parsing calls to appropriate
 web-site specific parser
 """
 from leetcodeParser import LeetCodeParser
+from geeksforgeeksParser import GeeksForGeeksParser
 
 
 class WebParser(object):
@@ -15,9 +16,20 @@ class WebParser(object):
         :return:
         """
         result_tag = None
+        try:
+            if not post.host_name:
+                print "No host name found in this post object"
+                return result_tag
+        except Exception:
+            print "No host name found in this post object"
+            return result_tag
+
         if 'leetcode' in post.host_name:
             result_tag = LeetCodeParser.parse(post)
+        elif 'geeksforgeeks' in post.host_name:
+            result_tag = GeeksForGeeksParser.parse(post)
         else:
-            print "I can't find any appropriate parser for this host. Ping that damn developer to patch me up!!!"
+            print "I can't find any appropriate parser for this host: " + post.host_name + \
+                  ". Ping that damn developer to patch me up!!!"
         return result_tag
 
